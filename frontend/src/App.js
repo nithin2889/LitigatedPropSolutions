7df795abba73947ff34d7a68d2beb57e60456b1c
akhilamartia <akhila.martia@gmail.com>
2020-12-31 03:05:36 +0530

    login screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 import CustomerScreen from "./screens/CustomerScreen";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/customer/:id" component={CustomerScreen} />
         </Container>
       </main>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,15 +5,37 @@ import {
   customerDetailsReducer,
   customerListReducer,
 } from "./reducers/customerReducers";
+import {
+  userDeleteReducer,
+  userDetailsReducer,
+  userListReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer,
+  userUpdateReducer,
+} from "./reducers/userReducers";
 
 // holds all the reducers of the application
 const reducer = combineReducers({
   customerList: customerListReducer,
   customerDetails: customerDetailsReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
 });
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 // loads initially when the redux store loads
-const initialState = {};
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 // thunk middleware
 const middleware = [thunk];
